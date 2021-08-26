74b0487541a5b8f99c75ee82612ed7a49bc21c18
tasha_dexter <tasha_dexter@mail.ru>
2021-08-26 13:07:02 +0300

    Fixed Tests architecture

---
+++ BankSystem/BankSystem.Tests/BankServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using BankSystem.Models;
+using BankSystem.Services;
+
+namespace BankSystem.Tests
+{
+    public class BankServiceTests
+    {
+        public class AddClientTests
+        {
+            [Fact]
+            public void AddClient_ID_VS1548_FN_Jack_LN_Smith_Age_21()
+            {
+                BankService bankService = new BankService();
+
+                Client client = new Client() { PassportID = "VS1548", FirstName = "Jack", LastName = "Smith", Age = 21 };
+                bankService.Add<Client>(client);
+
+                var result = bankService.Find<Client>(client.PassportID);
+
+                Assert.Equal(client, result);
+            }
+
+            [Fact]
+            public void AddClient_ID_VS1453_FN_Jenny_LN_Collin_Age_9_()
+            {
+                BankService bankService = new BankService();
+                Client client = new Client() { PassportID = "VS1453", FirstName = "Jenny", LastName = "Collin", Age = 9 };
+
+                bankService.Add<Client>(client);
+                var result = bankService.Find<Client>(client.PassportID);
+
+                Assert.Null(result);
+            }
+        }
+
+        [Fact]
+        public void FindClient_null()
+        {
+            var exception = typeof(NullReferenceException);
+            Type result = null;
+
+            BankService bankService = new BankService();
+            Client client = null;
+
+            try
+            {
+                bankService.Find<Client>(client.PassportID);
+            }
+            catch (Exception e)
+            {
+                result = e.GetType();
+            }
+            Assert.Equal(exception, result);
+        }
+
+        [Fact]
+        public void FindClient_ID_VP1588_FN_Nick_LN_Pane_Age_45()
+        {
+            Client client = new Client() { PassportID = "VP1588", FirstName = "Nick", LastName = "Pane", Age = 45 };
+            BankService bankService = new BankService();
+
+            bankService.Add<Client>(client);
+            IPerson result = bankService.Find<Client>(client.PassportID);
+
+            Assert.Equal(client, result);
+        }
+
+
+    }
+}
---
+++ BankSystem/BankSystem.Tests/ExchangeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BankSystem.Models;
+using BankSystem.Services;
+using Xunit;
+
+namespace BankSystem.Tests
+{
+    public class ExchangeTests
+    {
+        //positive test
+        [Fact]
+        public void ExchangeCurrency_ammount_125_ruble_0_014_dollar_1()
+        {
+
+            Exchange exchange = new Exchange();
+            Ruble ruble = new Ruble() { Type = "RUB", ValueInDollars = 0.014 };
+            Dollar dollar = new Dollar() { Type = "USD", ValueInDollars = 1 };
+
+            var result = exchange.ExchangeCurrency(125, ruble, dollar);
+
+            Assert.Equal(1.75, result);
+        }
+        //negative test
+        [Fact]
+        public void ExchangeCurrency_ammount_100_euro_1_19_ruble_0_014()
+        {
+
+            Exchange exchange = new Exchange();
+            Ruble ruble = new Ruble() { Type = "RUB", ValueInDollars = 0.014 };
+            Euro euro = new Euro() { Type = "EUR", ValueInDollars = 1.19 };
+
+            var result = exchange.ExchangeCurrency(125, euro, ruble);
+
+            Assert.Equal(10500, result);
+        }
+    }
+}
